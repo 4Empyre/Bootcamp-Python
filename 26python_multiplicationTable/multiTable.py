7fa58f287958aab12bc7959f4e79fb33fa9c0f84
Matthew Moe <38055340+4Empyre@users.noreply.github.com>
2018-06-07 03:13:26 -0700

    Add files via upload

---
+++ 26python_multiplicationTable/multiTable.py
@@ -0,0 +1,10 @@
+# INCOMPLETE
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12]
+num_list = [1,2,3,4,5,6,7,8,9,10,11,12]
+def multiTable(x, y):
+    print ("x", x)
+    for i in x:
+
+
+
+multiTable(numbers, num_list)
